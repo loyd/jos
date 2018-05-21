f461574e99cc25cf504f90635498dcdae9b2a4d5
Paul Loyd <pavelko95@gmail.com>
2018-05-21 08:18:19 +0300

    feat: allocate and map memory for envs

--- kern/pmap.c
+++ kern/pmap.c
@@ -149,7 +149,10 @@ mem_init(void)
 
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
-	// LAB 8: Your code here.
+
+	size_t envs_len = NENV * sizeof(struct Env);
+	envs = boot_alloc(envs_len);
+	memset(envs, 0, envs_len);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -180,7 +183,7 @@ mem_init(void)
 	// Permissions:
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
-	// LAB 8: Your code here.
+	boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR(envs), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
