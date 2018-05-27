80f674fd747c13e999c6631b3470902422d502e5
Paul Loyd <pavelko95@gmail.com>
2018-05-27 17:05:30 +0300

    feat: setup thisenv

--- lib/libmain.c
+++ lib/libmain.c
@@ -16,8 +16,14 @@ void
 libmain(int argc, char **argv)
 {
 	// set thisenv to point at our Env structure in envs[].
-	// LAB 8: Your code here.
-	thisenv = 0;
+	envid_t eid = sys_getenvid();
+
+	for (unsigned i = 0; i < NENV; ++i) {
+		if (envs[i].env_id == eid) {
+			thisenv = envs + i;
+			break;
+		}
+	}
 
 	// save the name of the program so that panic() can use it
 	if (argc > 0)
